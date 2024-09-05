08c3da3505c459bafc97136fb30d934508af4d93
dandreu7 <dandreu01@gmail.com>
2024-09-05 15:36:21 -0400

    Class modifications

---
+++ src/BookMaker.java
@@ -0,0 +1,3 @@
+public class BookMaker {
+    // TODO: Create a class that can be used to populate Bookshelf class and book folder
+}
--- src/FirstTimeSetUp.java
+++ src/FirstTimeSetUp.java
@@ -13,15 +13,29 @@ public class FirstTimeSetUp {
     public static void makeFolder() {
         // Create Base Folder "Library" if not exists
         File libFolder = new File(desktop + File.separator + "Library");
-        // TODO: make subfolders and .txt files that go in them for Library management
+        File bookFolder = new File(libFolder + File.separator + "Books");
+        File membersFolder = new File(libFolder + File.separator + "Members");
+        File moviesFolder = new File(libFolder + File.separator + "Movies");
+
+        // TODO: make subfolders and .txt files (make a new class that can be used for making books)
+        // that go in them for Library management
         if (libFolder.exists()) {
-            System.out.println("File 'Library' Exists Already");
+            System.out.println("File 'Library' Exists Already\nTo reset please delete folder and try again");
         } else {
             System.out.println("File 'Library' Does Not Exist");
-            System.out.println("Creating 'Library' Folder at " +
+            System.out.println("Creating 'Library' Folder and subsiquent subfolders at " +
                     desktop + File.separator + "Library");
             if(libFolder.mkdir()){
                 System.out.println("Library Folder created Successfully");
+                if(bookFolder.mkdir()){
+                    System.out.println("Book Folder created Successfully");
+                }
+                if(membersFolder.mkdir()){
+                    System.out.println("Member Folder created Successfully");
+                }
+                if(moviesFolder.mkdir()){
+                    System.out.println("Movie Folder created Successfully");
+                }
             } else {
                 System.out.println("Library Folder creation Failed");
             }
